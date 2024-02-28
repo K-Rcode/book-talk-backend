e949657e15a28fff67572cd7f919de2372dee0fa
kgittens89 <93796800+kgittens89@users.noreply.github.com>
2024-02-28 12:00:03 -0600

    heroku deploy

--- django_book_talk/settings.py
+++ django_book_talk/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default=get_random_secret_key())
+# SECRET_KEY = os.environ.get('SECRET_KEY', default=get_random_secret_key())
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ['MODE'] == 'dev' else False
